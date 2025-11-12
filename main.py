8c8b266e092a7e84e769f41ba50816046e35693d
Dima-crm28423 <107197602+Dima-crm28423@users.noreply.github.com>
2025-11-13 01:02:20 +0200

    revert

--- main.py
+++ main.py
@@ -100,37 +100,34 @@ def main():
     book = load_data(contacts_file)
 
     print("Welcome to the assistant bot!")
-    try:
-        while True:
-            user_input = input("Enter a command: ")
-            command, *args = parse_input(user_input)
+    while True:
+        user_input = input("Enter a command: ")
+        command, *args = parse_input(user_input)
 
-            match command:
-                case "hello":
-                    print("How can I help you?")
-                case "close" | "exit":
-                    save_data(book, contacts_file)
-                    print("Good bye!")
-                    break
-                case "add":
-                    print(add_contact(args, book))
-                case "change":
-                    print(change_contact(args, book))
-                case "phone":
-                    print(show_phone(args, book))
-                case "all":
-                    print(show_all_contacts(args, book))
-                case "add-birthday":
-                    print(add_birthday(args, book))
-                case "show-birthday":
-                    print(show_birthday(args, book))
-                case "birthdays":
-                    print(book.get_upcoming_birthdays())
-                case _:
-                    print("Invalid command.")
-    except KeyboardInterrupt:
-        save_data(book, contacts_file)
-        print("\nGood bye!")
+        match command:
+            case "hello":
+                print("How can I help you?")
+            case "close" | "exit":
+                save_data(book, contacts_file)
+                print("Good bye!")
+                break
+            case "add":
+                print(add_contact(args, book))
+            case "change":
+                print(change_contact(args, book))
+            case "phone":
+                print(show_phone(args, book))
+            case "all":
+                print(show_all_contacts(args, book))
+            case "add-birthday":
+                print(add_birthday(args, book))
+            case "show-birthday":
+                print(show_birthday(args, book))
+            case "birthdays":
+                print(book.get_upcoming_birthdays())
+            case _:
+                print("Invalid command.")
+
 
 if __name__ == "__main__":
     main()
